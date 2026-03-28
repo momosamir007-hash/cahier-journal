de1990ea4368ca5d69070d86154ee2379ac6adb4
momosamir007-hash <momosamir007@gmail.com>
2026-03-28 22:01:59 +0100

    Create __init__.py

---
+++ engine/__init__.py
@@ -0,0 +1,4 @@
+from .schedule import weekly_schedule, ROUTINE_ACTIVITIES, NAME_MAPPING
+from .extractor import extract_all_lessons
+from .template_builder import create_template_bytes
+from .injector import build_daily_planner_bytes
