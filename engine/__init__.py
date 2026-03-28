eb02c243dc83c89259570e18dc0614e2c4e2ad97
momosamir007-hash <momosamir007@gmail.com>
2026-03-28 22:48:42 +0100

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,4 +1,12 @@
-from .schedule import weekly_schedule, ROUTINE_ACTIVITIES, NAME_MAPPING
+from .schedule import (
+    weekly_schedule,
+    ROUTINE_ACTIVITIES,
+    NAME_MAPPING,
+    DOMAIN_MAPPING,
+    SUBJECT_WEEKLY_COUNT,
+    get_all_teaching_subjects,
+    get_domain_for,
+)
 from .extractor import extract_all_lessons
 from .template_builder import create_template_bytes
 from .injector import build_daily_planner_bytes
